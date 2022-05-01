96567d54cb35bb784bf8bafd5611db33940d3d80
João Palmeiro <joaommpalmeiro@gmail.com>
2022-05-01 08:23:03 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <main>
   <!-- https://tailwindcss.com/docs/height#viewport-height -->
   <!-- https://stackoverflow.com/questions/55056513/vertical-align-with-tailwind-css-across-full-screen-div -->
-  <div class="flex h-screen justify-center items-center">
+  <div class="flex flex-col h-screen justify-center items-center">
     <h1>Google Drive download link generator</h1>
   </div>
 </main>
