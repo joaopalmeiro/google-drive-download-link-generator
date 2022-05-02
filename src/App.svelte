69967d4c6b070e9cd92417a48e7235e37cdeb844
João Palmeiro <joaommpalmeiro@gmail.com>
2022-05-02 17:10:24 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
   $: downloadClasses =
     downloadUrl === undefined
       ? "text-gray-400 border-gray-400 bg-gray-50"
-      : "text-sendstack-onyx border-sendstack-onyx bg-gray-100";
+      : "text-sendstack-onyx border-sendstack-onyx bg-gray-50";
 </script>
 
 <!-- https://devdojo.com/tails -->
@@ -30,74 +30,80 @@
   <!-- https://stackoverflow.com/questions/55056513/vertical-align-with-tailwind-css-across-full-screen-div -->
   <!-- https://tailwindcss.com/docs/max-width -->
   <!-- https://tailwindcss.com/docs/letter-spacing#customizing-your-theme -->
-  <main class="max-w-screen-sm w-full p-9 my-auto bg-gray-50 rounded-3xl">
-    <!-- https://tailwindcss.com/docs/installation -->
-    <h1 class="text-sendstack-onyx text-xl tracking-tight font-bold">
-      Google Drive download link generator
-    </h1>
+  <main class="max-w-screen-sm w-full my-auto">
+    <div class="p-9 bg-gray-50 rounded-3xl">
+      <!-- https://tailwindcss.com/docs/installation -->
+      <h1 class="text-sendstack-onyx text-xl tracking-tight font-bold">
+        Google Drive download link generator
+      </h1>
 
-    <!-- https://tailwindcss.com/docs/align-items -->
-    <!-- https://tailwindcss.com/docs/gap -->
-    <form
-      on:submit|preventDefault={onSubmit}
-      class="flex flex-row items-center gap-x-4 mt-6"
-    >
-      <!-- https://www.hyperui.dev/components/inputs -->
-      <!-- https://www.hyperui.dev/components/forms -->
-      <!-- https://flowbite.com/docs/forms/input-field/ -->
-      <!-- https://tailwindcss.com/docs/screen-readers -->
-      <!-- https://tailwindcss.com/docs/top-right-bottom-left -->
-      <!-- https://egghead.io/lessons/svelte-creating-a-simple-form-with-svelte -->
-      <!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/url -->
-      <!-- https://regex101.com/ https://regex101.com/r/ZQbU2n/1 -->
-      <!-- https://stackoverflow.com/questions/23224590/regex-to-modify-google-drive-shared-file-url -->
-      <div class="flex-1">
-        <label for="url" class="sr-only">Google Drive link</label>
-        <div class="relative">
-          <input
-            type="url"
-            name="url"
-            id="url"
-            pattern="https://drive\.google\.com/file/d/.*?/view\?usp=sharing"
-            class="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm placeholder:text-gray-400 text-sendstack-onyx"
-            placeholder="https://drive.google.com/file/d/<ID>/view?usp=sharing"
-            required
-            bind:value={url}
-          />
-          <!-- https://tailwindcss.com/docs/display#inline-flex -->
-          <span class="absolute inset-y-0 inline-flex items-center right-4">
-            <!-- https://heroicons.com/ -->
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-5 h-5 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-              />
-            </svg>
-          </span>
-        </div>
-      </div>
+      <p class="mt-1 text-base text-sendstack-gemstone">
+        Paste a file share link and get a direct download link.
+      </p>
 
-      <!-- https://tailwindcss.com/docs/hover-focus-and-other-states -->
-      <!-- https://www.hyperui.dev/components/buttons -->
-      <!-- https://tailwindcss.com/docs/display#block-and-inline -->
-      <!-- Note: The text color also changes when hovering, not just the background. -->
-      <!-- https://tailwindcss.com/docs/hover-focus-and-other-states#hover-focus-and-active -->
-      <button
-        type="submit"
-        class="bg-sendstack-one hover:bg-sendstack-two text-sendstack-onyx hover:text-sendstack-gemstone px-4 py-2 text-sm font-medium leading-6 rounded-full"
-        >Generate</button
+      <!-- https://tailwindcss.com/docs/align-items -->
+      <!-- https://tailwindcss.com/docs/gap -->
+      <form
+        on:submit|preventDefault={onSubmit}
+        class="flex flex-row items-center gap-x-4 mt-6"
       >
-    </form>
+        <!-- https://www.hyperui.dev/components/inputs -->
+        <!-- https://www.hyperui.dev/components/forms -->
+        <!-- https://flowbite.com/docs/forms/input-field/ -->
+        <!-- https://tailwindcss.com/docs/screen-readers -->
+        <!-- https://tailwindcss.com/docs/top-right-bottom-left -->
+        <!-- https://egghead.io/lessons/svelte-creating-a-simple-form-with-svelte -->
+        <!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/url -->
+        <!-- https://regex101.com/ https://regex101.com/r/ZQbU2n/1 -->
+        <!-- https://stackoverflow.com/questions/23224590/regex-to-modify-google-drive-shared-file-url -->
+        <div class="flex-1">
+          <label for="url" class="sr-only">Google Drive link</label>
+          <div class="relative">
+            <input
+              type="url"
+              name="url"
+              id="url"
+              pattern="https://drive\.google\.com/file/d/.*?/view\?usp=sharing"
+              class="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm placeholder:text-gray-400 text-sendstack-onyx"
+              placeholder="https://drive.google.com/file/d/<ID>/view?usp=sharing"
+              required
+              bind:value={url}
+            />
+            <!-- https://tailwindcss.com/docs/display#inline-flex -->
+            <span class="absolute inset-y-0 inline-flex items-center right-4">
+              <!-- https://heroicons.com/ -->
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
 
-    <div class="flex flex-row items-center gap-x-4 mt-4">
+        <!-- https://tailwindcss.com/docs/hover-focus-and-other-states -->
+        <!-- https://www.hyperui.dev/components/buttons -->
+        <!-- https://tailwindcss.com/docs/display#block-and-inline -->
+        <!-- Note: The text color also changes when hovering, not just the background. -->
+        <!-- https://tailwindcss.com/docs/hover-focus-and-other-states#hover-focus-and-active -->
+        <button
+          type="submit"
+          class="bg-sendstack-one hover:bg-sendstack-two text-sendstack-onyx hover:text-sendstack-gemstone px-4 py-2 text-sm font-medium leading-6 rounded-full"
+          >Generate</button
+        >
+      </form>
+    </div>
+
+    <div class="flex flex-row items-center gap-x-4 mt-4 px-9">
       <!-- https://www.hyperui.dev/components/alerts -->
       <!-- https://tailwindcss.com/docs/text-overflow -->
       <!-- https://tailwindcss.com/docs/overflow -->
